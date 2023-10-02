961234886059fb067ab38d785d13ca9198219ece
FigueroaF <figueroaf1939w@gmail.com>
2023-10-02 01:13:31 -0300

    :sparkles: filter function

---
+++ src/filtrar_tabla.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'clases/RepositorioGastos.php';
+
+
+$filtro = $_POST['filtro'];
+$repositorioGastos = new RepositorioGastos();
+$resultadosFiltrados = $repositorioGastos->filtrarCat($filtro);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Mostrar Gastos</title>
+        <link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body class="container">
+<?php include('navbar.php') ?>
+<?php
+
+    if ($resultadosFiltrados) {
+
+        echo "<table border='1'>
+                <tr>
+                    <th>Fecha</th>
+                    <th>Descripcion del gasto</th>
+                </tr>";
+
+        while ($row = $resultadosFiltrados->fetch_assoc()) {
+            echo "<tr>
+                    <td>" . $row['fecha'] . "</td>
+                    <td>" . $row['descripcion'] . "</td>
+                  </tr>";
+        }
+
+        echo "</table>";
+    } else {
+
+        echo "No se encontraron resultados para el filtro: $filtro";
+    }
+    ?>
+
+</body>
+</html>
