a7b2efc44b9fe29f37074422c9cb689a0fae048a
Franco Yunes <franco.yunes@hotmail.com>
2023-10-10 22:07:53 -0300

    :sparkles: Add .envPULICO that was missing

---
+++ src/.envPUBLICO.php
@@ -0,0 +1,10 @@
+<?php
+
+function credenciales() {
+    return [
+        'usuario' => 'juan',
+        'clave' => 'supersecreto',
+        'servidor' => 'localhost',
+        'base_de_datos' => 'conexion_php',
+    ];
+}
